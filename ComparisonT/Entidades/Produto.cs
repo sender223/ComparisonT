a6308c8e74badcdcbc87c7bb5adb6f5e051a01b3
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2020-03-17 12:20:51 -0300

    Utilizando o IComparable para ordenar os itens em ordem alfabetica, com isso a classe não fica fechada para alteração, precisando alterar toda vez que mudar o criterio de comparação

---
+++ ComparisonT/Entidades/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ComparisonT.Entidades {
+    //usando o Icomparable 
+    class Produto : IComparable<Produto> {
+
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        //construtor
+        public Produto(string nome, double preco) {
+            Nome = nome;
+            Preco = preco;
+        }
+        //metodo tostring para transformar a classe em string
+        public override string ToString() {
+            return Nome + ", " + Preco.ToString("F2", CultureInfo.InvariantCulture);
+        }
+        //metodo da operação Para comparar.
+        public int CompareTo(Produto outro) {
+            //aqui se quisermos trocar o metodo de comparação de nome para preco
+            //precisamos alterar a linha abaixo. com isso a classe não fica fechada.
+            return Nome.ToUpper().CompareTo(outro.Nome.ToUpper());
+        }
+
+
+    }
+}
--- ComparisonT/Program.cs
+++ ComparisonT/Program.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using ComparisonT.Entidades;
+using System.Collections.Generic;
 
 namespace ComparisonT {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            List<Produto> lista = new List<Produto>();
+
+            lista.Add(new Produto("TV", 900.00));
+            lista.Add(new Produto("Notebook", 1200.00));
+            lista.Add(new Produto("Tablet", 450.00));
+
+            lista.Sort();
+
+            foreach (Produto p in lista) {
+                Console.WriteLine(p);
+            }
+            
         }
     }
 }
